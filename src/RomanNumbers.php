2dcdd5af8cb88a5782259b77820add014af3b2da
David Martinez <davidmgilo@gmail.com>
2016-10-25 16:02:06 +0200

    protected code

--- src/RomanNumbers.php
+++ src/RomanNumbers.php
@@ -2,17 +2,25 @@
 
 class RomanNumbers
 {
+    protected $code = [
+            1 => "I",
+            4 => "IV",
+            5 => "V",
+            9 => "IX",
+            10 => "X",
+        ];
+
+
     public function convert($number){
 
         $solution = "";
 
-        if($number == 10){
-            $solution = 'X';
-        } elseif ($number == 5) {
-            $solution ='V';
-        } else {
-            $solution = $solution . str_repeat('I',$number);
+        if($number == 4) {
+            $solution = $this->code[$number];
+            $number = 0;
         }
+
+        $solution = $solution . str_repeat('I',$number);
 
         return $solution;
 
