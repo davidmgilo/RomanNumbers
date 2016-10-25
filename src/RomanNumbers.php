7857a626ca2de7f8bd43188c5cf350bd9c1acac5
David Martinez <davidmgilo@gmail.com>
2016-10-25 15:53:09 +0200

    var solution

--- src/RomanNumbers.php
+++ src/RomanNumbers.php
@@ -3,14 +3,18 @@
 class RomanNumbers
 {
     public function convert($number){
+
+        $solution = "";
+
         if($number == 10){
-            return 'X';
-        }
-        if($number == 5){
-            return 'V';
+            $solution = 'X';
+        } elseif ($number == 5) {
+            $solution ='V';
+        } else {
+            $solution = $solution . str_repeat('I',$number);
         }
 
-        return str_repeat('I',$number);
+        return $solution;
 
     }
 }
